7681911dec8fdeafc36066f7dca6dc9a24fb9abc
MrGullman <jesper.gullman@outlook.com>
2016-10-20 21:29:51 +0200

    Inlämnad
    Första versionen inlämnad till lärare för en feedback

--- js/guess_game.js
+++ js/guess_game.js
@@ -1,42 +1,74 @@
-var ownNumber = parseInt(prompt("Vilket är högsta nummret?"));
+// Startar spelet genom att fråga spelaren vilket som den vill skall vara högsta nummret
+var ownTopNumber = parseInt(prompt("Vilket är högsta nummret?"));
+
+// Välj hur många försök man behöver på sig
+var diffeculty = parseInt(prompt("Hur många försök vill du ha på dig?"));
+
 
 // deklarerar variablerna
-var upperNumber = ownNumber;
+var upperNumber = ownTopNumber;
 var randomNumber = getRandomNumber(upperNumber);
 var guess;
 var attempt = 0;
+var numTry = diffeculty;
 
 
-// en funktion som skapar ett random nummer med
+// en funktion som skapar ett random nummer
 function getRandomNumber(upperNumber){
   return Math.floor(Math.random() * upperNumber ) +1;
 }
 
 
-console.log(typeof ownNumber);
-//guess;
-//console.log(guess);
-console.log(randomNumber);
 
-while (guess !== randomNumber){
-  guess = parseInt(prompt("Gissa ett nummer mellan 1 & " + upperNumber));//getRandomNumber(upperNumber);
+// kod för att lättare kunna gissa rätt nummer när man utvecklar
+/*console.log(typeof ownTopNumber);
+
+console.log(randomNumber);
+console.log(typeof numTry);*/
+
+
+
+// while loop som fortsätter att köra tills att man har gissar rätt
+
+while (guess !== randomNumber && numTry > 0){
+  
+  // Antalet försök man har på sig
+  numTry -=1;
+  
+  
+  guess = parseInt(prompt("Gissa ett nummer mellan 1 & " + upperNumber));
   
   if(guess === randomNumber){
     alert("Grattis det va rätt");
-    console.log("Grattis det va rätt");
+    //console.log("Grattis det va rätt");
   }else if(guess < randomNumber){
-    alert("Talet är större!");
-    console.log("talet är större");
+    alert("Talet är större!, du har " + numTry + " försök kvar.");
+    //console.log("talet är större");
   }else{
-    alert("Talet är mindre!");
-    console.log("Talet är mindre!");
+    alert("Talet är mindre!, du har " + numTry + " försök kvar.");
+    //console.log("Talet är mindre!");
   }
+ 
+  // Loggar hur många försök man har behövt på sig.
   attempt += 1;
+  
+  
+  /*console.log("Antal försök: " + numTry);*/
+
+}
+ 
+
+
+// Här kollar vi om det är försöken som är slut eller om du hade rätt.
+
+//console.log("Antal försök: " + numTry);
+if(numTry > 0){
+  alert("Kanon du gissade rätt. Du behövde " + attempt + " försök för att gissa rätt.");
+//console.log(attempt);
+}else{
+  alert("Tyvärr du förlorade du har inga fler försök.");
 }
 
-console.log(attempt);
-//alert("Välkommen till Jespers Nummergissnings Spel");
 
 
 
-//annars visar det fel
