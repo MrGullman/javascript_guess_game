2220992f737daccdfe61750c7de80dc461f648af
MrGullman <jesper.gullman@outlook.com>
2016-10-21 17:56:13 +0200

    Uppdatering
    Efter samtal med lärare, har jag uppdaterat en del kod, samt lagt till
    så man kan få antal försök beroende på val av svårighetsgrad.

--- js/guess_game.js
+++ js/guess_game.js
@@ -1,16 +1,53 @@
+var playAgain = true;
+
+while(playAgain){
+
 // Startar spelet genom att fråga spelaren vilket som den vill skall vara högsta nummret
 var ownTopNumber = parseInt(prompt("Vilket är högsta nummret?"));
 
 // Välj hur många försök man behöver på sig
-var diffeculty = parseInt(prompt("Hur många försök vill du ha på dig?"));
 
+var diffeculty = parseInt(prompt("Välj Svårighetsgrad mellan 1 - 5, var 1 är lättast."));
+  
+// Kollar om diffeculty är mellan 1-5
+while (diffeculty !== 1 && diffeculty !== 2 && diffeculty !== 3 && diffeculty !== 4 && diffeculty !== 5){
+  diffeculty = parseInt(prompt("Välj en svårighet mellan 1 - 5"));
+}
 
 // deklarerar variablerna
 var upperNumber = ownTopNumber;
 var randomNumber = getRandomNumber(upperNumber);
 var guess;
 var attempt = 0;
-var numTry = diffeculty;
+var numTry = setNumberOfTries(diffeculty);
+
+
+// Funktion för att sätta antalet försök baserat på svårighetsgrad.
+function setNumberOfTries(diffeculty){
+  var numTry = 20;
+  
+  switch(diffeculty) {
+    case 1:
+      numTry = 20;
+      break;
+    case 2:
+      numTry = 15;
+      break;
+    case 3:
+      numTry = 10;
+      break;
+    case 2:
+      numTry = 6;
+      break;
+    case 5:
+      numTry = 3;
+      break;
+    default:
+      console.log("Du måste välja ett värde mellan 1-5");
+  }
+  
+  return numTry;
+}
 
 
 // en funktion som skapar ett random nummer
@@ -21,11 +58,12 @@ function getRandomNumber(upperNumber){
 
 
 // kod för att lättare kunna gissa rätt nummer när man utvecklar
-/*console.log(typeof ownTopNumber);
+console.log(typeof ownTopNumber);
 
 console.log(randomNumber);
-console.log(typeof numTry);*/
+console.log(typeof numTry);
 
+// Tar bort ett försök för det första försöket man gör.
 
 
 // while loop som fortsätter att köra tills att man har gissar rätt
@@ -35,40 +73,43 @@ while (guess !== randomNumber && numTry > 0){
   // Antalet försök man har på sig
   numTry -=1;
   
-  
   guess = parseInt(prompt("Gissa ett nummer mellan 1 & " + upperNumber));
   
   if(guess === randomNumber){
     alert("Grattis det va rätt");
-    //console.log("Grattis det va rätt");
+
   }else if(guess < randomNumber){
-    alert("Talet är större!, du har " + numTry + " försök kvar.");
-    //console.log("talet är större");
+    if(numTry>0){
+      alert("Talet är större!, du har " + numTry + " försök kvar.");
+    }else{
+      alert("Dina försök är slut.")
+    }
   }else{
-    alert("Talet är mindre!, du har " + numTry + " försök kvar.");
-    //console.log("Talet är mindre!");
+    if (numTry>0){
+      alert("Talet är mindre!, du har " + numTry + " försök kvar.");
+    }else{
+      alert("Dina försök är slut.")
+    }
   }
  
   // Loggar hur många försök man har behövt på sig.
   attempt += 1;
   
-  
-  /*console.log("Antal försök: " + numTry);*/
-
 }
  
-
-
 // Här kollar vi om det är försöken som är slut eller om du hade rätt.
 
-//console.log("Antal försök: " + numTry);
 if(numTry > 0){
   alert("Kanon du gissade rätt. Du behövde " + attempt + " försök för att gissa rätt.");
-//console.log(attempt);
 }else{
-  alert("Tyvärr du förlorade du har inga fler försök.");
+  alert("GAME OVER! Rätt tal va: " + randomNumber);
 }
 
+var again = prompt("Vill Du Spela Igen: Y / N");
 
-
-
+  if(again === "Y"){
+    playAgain = true;
+  }else{
+    playAgain = false;
+  }
+}
